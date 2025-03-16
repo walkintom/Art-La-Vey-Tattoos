6d89e8c3db58e7603395e9c86a20545a4b187bfd
Tom Case <tom.case@icloud.com>
2025-03-16 21:41:20 +0000

    Removed Messenger plugin code

--- js/main.js
+++ js/main.js
@@ -123,23 +123,3 @@ fetch(event.target.action, {
 });
 }
 form.addEventListener("submit", handleSubmit)
-
-// Messenger Plugin
-var chatbox = document.getElementById('fb-customer-chat');
-chatbox.setAttribute("page_id", "686759324672307");
-chatbox.setAttribute("attribution", "biz_inbox");
-
-window.fbAsyncInit = function() {
-  FB.init({
-    xfbml            : true,
-    version          : 'v13.0'
-  });
-};
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_GB/sdk/xfbml.customerchat.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
